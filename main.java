e97156d04f6ccfd3d4dd4451367d52c88e1128d2
georgefl1 <36734486+georgefl1@users.noreply.github.com>
2019-12-05 13:16:34 -0800

    Added updates

--- main.java
+++ main.java
@@ -3,18 +3,43 @@
  *
  * @author George Lord 2019
  */
+import java.io.*;
 class Main 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)throws IOException
     { 
-        try
-        { 
-           Process process = Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"echo You can find the current known devices and their assigned IP and MAC adresses on your network below: && arp -a\"");
-        } 
-        catch (Exception e) 
-        { 
-            System.out.println("Hey, something went wrong and we can't seem to open CMD. Please try again."); 
-            e.printStackTrace(); 
-        } 
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("C:\\Users\\basic\\OneDrive\\Desktop\\Wifiscript\\src\\main\\java\\Script.bat");
+        
+        try {
+
+		Process process = processBuilder.start();
+
+		StringBuilder output = new StringBuilder();
+
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(process.getInputStream()));
+
+		String line;
+                int count = 0;
+		while ((line = reader.readLine()) != null) {
+			output.append(line + "\n");
+                        if (line.contains("static") || line.contains("dynamic")){
+                            count++;
+                        }
+		}
+                Process p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"echo There are " + count + " visible online devices connected to this network && PAUSE\"");
+        
+
+		int exitVal = process.waitFor();
+		if (exitVal == 0) {
+			System.exit(0);
+		}
+
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
     } 
 } 
