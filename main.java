2a439bfa289e1348608c03f8d1ec3978e96570ee
georgefl1 <36734486+georgefl1@users.noreply.github.com>
2019-12-06 13:44:12 -0800

    Update main.java

--- main.java
+++ main.java
@@ -4,12 +4,14 @@
  * @author George Lord 2019
  */
 import java.io.*;
+import java.util.*;
+import java.net.*;  
+
 class Main 
 {
-    public static void main(String[] args)throws IOException
-    { 
+    public static String getName(String ip){
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("C:\\Users\\basic\\OneDrive\\Desktop\\Wifiscript\\src\\main\\java\\Script.bat");
+        processBuilder.command("cmd.exe", "/c", "nslookup " + ip);
         
         try {
 
@@ -21,25 +23,49 @@ class Main
 				new InputStreamReader(process.getInputStream()));
 
 		String line;
+                ArrayList<String> lines = new ArrayList<String>();
+                
                 int count = 0;
 		while ((line = reader.readLine()) != null) {
-			output.append(line + "\n");
-                        if (line.contains("static") || line.contains("dynamic")){
-                            count++;
-                        }
+			lines.add(line);
 		}
-                Process p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"echo There are " + count + " visible online devices connected to this network && PAUSE\"");
-        
-
-		int exitVal = process.waitFor();
-		if (exitVal == 0) {
-			System.exit(0);
-		}
+                
+                if (lines.size() >=4 ){
+                    return lines.get(3);
+                }
+                else{
+                    return ip;
+                }
+                
+                
 
 	} catch (IOException e) {
 		e.printStackTrace();
-	} catch (InterruptedException e) {
-		e.printStackTrace();
-	}
+        }
+        return "ERROR";
+        
+    }
+    public static void main(String[] args)throws IOException
+    {
+        ArrayList<String> ips = new ArrayList<String>();
+        
+        String sub = "192.168.1";
+        
+        System.out.println("Scanning IPs. Please allow up to five minutes for this process.");
+        
+        for (int i=1;i<255;i++){
+            
+            String ip=sub + "." + i;
+            
+            if (InetAddress.getByName(ip).isReachable(1000)){
+                ips.add(ip);
+            }
+        }
+        
+        System.out.println("We found " + ips.size() + " devices on your subnet. Here are their names and IPs:");
+        for (String i: ips){
+            System.out.println(getName(i) + " found at IP " + i);
+        }
+        
     } 
 } 
